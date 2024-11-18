b872fdb0232a2db3b377f96cc283fb879508e26c
mezter1337 <andreas.javert@gmail.com>
2024-11-18 14:12:19 +0100

    Add Post model

--- home/models.py
+++ home/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-from django.contrib.auth import user_logged_in
+from django.contrib.auth.models import User
+
+IS_PUBLISHED = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200, unique=True, blank=False)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    is_published = models.IntegerField(choices=IS_PUBLISHED, default=0)
