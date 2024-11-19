f25fd2e069ec233333f4381646e36be89a330b79
mezter1337 <andreas.javert@gmail.com>
2024-11-19 12:46:31 +0100

    Fix hopefully to be able to load css

--- main/settings.py
+++ main/settings.py
@@ -168,9 +168,13 @@ LOGIN_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+# Static
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Local static files
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Whitenoise
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
