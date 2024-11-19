961c381cc3641968cc3c7e9af41e9f70a69988b1
mezter1337 <andreas.javert@gmail.com>
2024-11-19 08:48:22 +0100

    Install tinymce

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ class PostList(ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
 
 
-class PostDetail(TemplateView):
+class PostDetail(ListView):
     model = Post
     template_name = "blog/post_detail.html"
+    queryset = Post.objects.filter(status=1)
--- main/settings.py
+++ main/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     # Others
     "crispy_forms",
     "crispy_bootstrap5",
+    "tinymce",
 ]
 
 SITE_ID = 1
