a039874696a5ecb42e13ee66186ab0484bb4074d
mezter1337 <andreas.javert@gmail.com>
2024-11-20 19:33:52 +0100

    Updated edit function, populated with json and edit works both from the card and from the detail view

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,11 @@ from .views import PostList, post_detail, CreatePost, EditPost, DeletePost
 urlpatterns = [
     path("", PostList.as_view(), name="home"),
     path("post/<slug:slug>/", post_detail, name="post_detail"),
-    path("post/new", CreatePost.as_view(), name="post_create"),
+    path("post/create", CreatePost.as_view(), name="post_create"),
     path("post/<slug:slug>/edit/", EditPost.as_view(), name="post_edit"),
+    path(
+        "post/<slug:slug>/post_delete/<int:pk>/",
+        DeletePost.as_view(),
+        name="post_delete",
+    ),
 ]
