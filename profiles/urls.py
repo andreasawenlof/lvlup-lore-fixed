1b43aa70dcfbd4bcec14c7ebf03c99be20bd2c9b
mezter1337 <andreas.javert@gmail.com>
2024-11-21 18:11:34 +0100

    Add users being able to see what they've commented on

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Profiles
 
 urlpatterns = [
-    path("user/<slug:pk>/", Profiles.as_view(), name="profile"),
+    path("user/<int:pk>/", Profiles.as_view(), name="profile"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.views.generic import TemplateView, UpdateView
 from .forms import ProfileForm
 from .models import Profile
+from blog.models import Post, Comment
 
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -11,9 +13,15 @@ class Profiles(TemplateView):
     template_name = "profiles/profile.html"
 
     def get_context_data(self, **kwargs):
-        profile = Profile.objects.get(user=self.kwargs["pk"])
+        user = User.objects.get(id=self.kwargs["pk"])
+        profile = Profile.objects.get(user=user)
+
+        # Get all posts that the user has commented on
+        commented_posts = Post.objects.filter(comments__author=user).distinct()
+
         context = {
             "profile": profile,
+            "commented_posts": commented_posts,  # Add the posts to the context
         }
 
         return context
