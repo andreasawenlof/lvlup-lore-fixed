c39776f52405200b37bc0d19e4750f2ca51c010d
mezter1337 <andreas.javert@gmail.com>
2024-11-21 17:48:17 +0100

    Created page for profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, UpdateView
+from .forms import ProfileForm
 from .models import Profile
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 class Profiles(TemplateView):
@@ -15,3 +17,19 @@ class Profiles(TemplateView):
         }
 
         return context
+
+
+class EditProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Edit Profile View"""
+
+    template_name = "profiles/edit_profile.html"
+    model = Profile
+    form_class = ProfileForm
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        self.success_url = f"/profile/view/{self.kwargs['pk']}"
+        return super().form_valid(form)
+
+    def test_func(self):
+        return self.request.user == self.get_pbject().user
