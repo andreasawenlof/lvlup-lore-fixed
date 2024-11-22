8ceaffdecc47b47b0e08d397e25346e0af75a73c
mezter1337 <andreas.javert@gmail.com>
2024-11-22 04:23:00 +0100

    change for avatar to be blank

--- profiles/models.py
+++ profiles/models.py
@@ -17,7 +17,7 @@ class Profile(models.Model):
         quality=75,
         upload_to="profiles/",
         force_format="WEBP",
-        blank=False,
+        blank=True,
     )
     bio = models.TextField(max_length=2500, null=True, blank=True)
 
